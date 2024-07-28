6b130b6fa461550210dc31c8f648441ff2ff5e9b
shudhanshu3367 <48749694+shudhanshu3367@users.noreply.github.com>
2024-07-28 11:33:29 +0530

    Add files via upload

---
+++ MarsRoverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utydevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
